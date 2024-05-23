2fdac851a1b46a758692fd88a4228075de3e06b0
lealemao <leonjetix126@gmail.com>
2024-05-22 22:09:39 -0300

    Finalizado AccountController e criado login.

--- GalloFlix/Controllers/AccountController.cs
+++ GalloFlix/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 using System.Net.Mail;
+using System.Security.Claims;
 using GalloFlix.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -37,17 +38,48 @@ public class AccountController : Controller
     {
         if (ModelState.IsValid)
         {
+            string userName = login.Email;
+            if (IsValidEmail(userName)) 
+            {
+                var user = await _userManager.FindByEmailAsync(userName);
+                if (user != null)
+                    userName = user.UserName;
+            }
 
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Password, login.RememberMe, lockoutOnFailure: true
+            );
+
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário {userName} acessou o sistema.");
+                return LocalRedirect(login.ReturnUrl);
+            }
+
+            if (result.IsLockedOut)
+            {
+                _logger.LogWarning($"Usuário {userName} foi bloqueado.");
+                ModelState.AddModelError(string.Empty, "Conta Bloqueada! aguarde alguns minutos para tentar novamente.");
+            }
+            else
+                ModelState.AddModelError(string.Empty, "Usuário e/ou Senha Inválidos!");
         }
         return View(login);
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout()
     {
-        return View("Error!");
+        _logger.LogInformation($"Usuário {ClaimTypes.Email} fez logoff");
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
     }
 
+    public IActionResult AccessDenied()
+    {
+        return View();
+    }
     private static bool IsValidEmail(string email)
     {
         try
